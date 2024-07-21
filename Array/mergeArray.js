bd77b649cb7613875b5d6dadded75aa66c0e6092
Nitin Rajput <128917704+nitinrajputind@users.noreply.github.com>
2024-07-22 00:05:44 +0530

    merge arrays

---
+++ Array/mergeArray.js
@@ -0,0 +1,12 @@
+let arr1 = [3, 7, 8, 9, 10, 11];
+let arr2 = [1, 2, 3, 4, 5, 6];
+let arr3 = [];
+
+for (let i = 0; i < arr1.length; i++) {
+  arr3.push(arr1[i]);
+}
+for (let i = 0; i < arr2.length; i++) {
+  arr3.push(arr2[i]);
+}
+
+console.log(arr3);
